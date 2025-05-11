87ec854ac22cddafb56dbc3d680346f8b29dda28
Hardicine <hardicine@mail.ru>
2025-05-11 17:30:02 +0300

    11.05. Вроде всё

--- CLab_4_1/Lab_4_1.c
+++ CLab_4_1/Lab_4_1.c
@@ -2,20 +2,30 @@
 #include <Windows.h>
 #define MAXSIZE 40
 
-char* DeleteQuestionMark(char* str)
+void Shift(char** str, int i)
+{
+	int j = 0;
+	for (; *(*str + i + j) != '\0'; j++)
+	{
+		*(*str + i + j) = *(*str + i + j + 1);
+	}
+	*str = (char*)realloc(*str, i + j);
+}
+void DeleteQuestionMark(char* str)
 {
 	for (int i = 1; *(str + i) != '\0'; i++)
 	{
 		if (*(str + i - 1) == 'а' && *(str + i) == '?')
 		{
-			*(str + i) = ' ';
+			Shift(&str, i);
+			i--;
 		}
 	}
 }
 void CountOfPO(char* str)
 {
 	int count = 0;
-	for (int i = 1; *(str + i) != '\0'; i++)
+	for (int i = 0; *(str + i) != '\0'; i++)
 	{
 		if (*(str + i) == 'Р' || *(str + i) == 'О')
 		{
@@ -24,23 +34,33 @@ void CountOfPO(char* str)
 	}
 	printf_s("Общее количество 'Р' и 'О' = %d\n", count);
 }
-
-int main()
+char* InputStr()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
 	char* str = (char*)calloc(MAXSIZE, sizeof(char));
 	if (str == NULL)
 	{
 		puts("Ошибка памяти !");
 		return 1;
 	}
-	gets_s(str, MAXSIZE);
+	int i = 0;
+	while ((*(str + i++) = (char)getchar()) != '\n');
+	*(str + --i) = '\0';
+	str = (char*)realloc(str, i + 1);
+	return str;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char* str = NULL;
+	puts("Введите строку:");
+	str = InputStr();
 
 	DeleteQuestionMark(str);
 	CountOfPO(str);
-	printf_s("%s\n", str);
+	puts(str);
 
 	free(str);
 	system("pause");
--- CLab_4_2/Lab_4_2.c
+++ CLab_4_2/Lab_4_2.c
@@ -1,49 +1,95 @@
-#include <stdio.h>
+Ôªø#include <stdio.h>
 #include <Windows.h>
 #include <string.h>
 #define MAXSIZE 40
 
-char* DeleteQuestionMark(char* str)
+void Shift(char** str, int FirstWordLen, int len, int totalShift)
 {
-	for (int i = 1; *(str + i) != '\0'; i++)
-	{
-		if (*(str + i - 1) == '‡' && *(str + i) == '?')
-		{
-			*(str + i) = ' ';
-		}
-	}
+    for (int j = len; j >= FirstWordLen; j--)
+    {
+        *(*str + j + totalShift) = *(*str + j);
+    }
 }
-void CountOfPO(char* str)
+void WordsWithoutE(char* str)
 {
-	int count = 0;
-	for (int i = 1; *(str + i) != '\0'; i++)
-	{
-		if (*(str + i) == '–' || *(str + i) == 'Œ')
-		{
-			count++;
-		}
-	}
-	printf_s("Œ·˘ÂÂ ÍÓÎË˜ÂÒÚ‚Ó '–' Ë 'Œ' = %d\n", count);
+    int isWord = 0;
+    int isWithE = 0;
+    int counter = 0;
+    for (int i = 0; *(str + i) != '\0'; i++)
+    {
+        if (isWord == 0 && *(str + i) != ' ')
+        {
+            isWord = 1;
+        }
+        if (isWord && (*(str + i) == '–ï'))
+        {
+            isWithE = 1;
+        }
+        if (*(str + i) == ' ' || *(str + i + 1) == '\0')
+        {
+            if (isWord)
+            {
+                if (isWithE == 0)
+                {
+                    counter++;
+                }
+                isWithE = 0;
+            }
+            isWord = 0;
+        }
+    }
+    printf_s("–ö–æ–ª–∏—á–µ—Å—Ç–≤–æ —Å–ª–æ–≤ –±–µ–∑ –±—É–∫–≤—ã –ï = %d\n", counter);
+}
+char* InputStr()
+{
+    char* str = (char*)calloc(MAXSIZE, sizeof(char));
+    if (str == NULL)
+    {
+        puts("–û—à–∏–±–∫–∞ –ø–∞–º—è—Ç–∏ !");
+        return NULL;
+    }
+    int i = 0;
+    while ((*(str + i++) = (char)getchar()) != '\n' && i < MAXSIZE - 1);
+    *(str + --i) = '\0';
+    str = (char*)realloc(str, i + 1);
+    return str;
+}
+
+void RepeatFirstWord(char** str)
+{
+    int len = strlen(*str);
+    int FirstWordLen = 0;
+    while (*(*str + FirstWordLen) != ' ' && *(*str + FirstWordLen) != '\0')
+    {
+        FirstWordLen++;
+    }
+    int repeats = 3;
+    int totalShift = (FirstWordLen + 1) * (repeats - 1);
+    *str = (char*)realloc(*str, len + totalShift + 1);
+    Shift(str, FirstWordLen, len, totalShift);
+    for (int i = 1; i < repeats; i++)
+    {
+        *(*str + FirstWordLen * i + i - 1) = ' ';
+        for (int j = 0; j < FirstWordLen; j++)
+        {
+            *(*str + FirstWordLen * i + i + j) = *(*str + j);
+        }
+    }
 }
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	char* str = (char*)calloc(MAXSIZE, sizeof(char));
-	if (str == NULL)
-	{
-		puts("Œ¯Ë·Í‡ Ô‡ÏˇÚË !");
-		return 1;
-	}
-	gets_s(str, MAXSIZE);
+    char* str = NULL;
+    puts("–í–≤–µ–¥–∏—Ç–µ —Å—Ç—Ä–æ–∫—É:");
+    str = InputStr();
+    WordsWithoutE(str);
+    RepeatFirstWord(&str);
+    puts(str);
 
-	DeleteQuestionMark(str);
-	CountOfPO(str);
-	printf_s("%s\n", str);
-
-	free(str);
-	system("pause");
-	return 0;
+    free(str);
+    system("pause");
+    return 0;
 }
